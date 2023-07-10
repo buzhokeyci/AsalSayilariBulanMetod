bf93cec2557bcec127685d8c8d786a48adbc343d
Enes Mert Kaya <127692367+buzhokeyci@users.noreply.github.com>
2023-07-10 10:47:05 +0300

    AsalSayilariBulanMetod
    girilen aralikta asal sayilari bulur.

---
+++ AsalSayılarıBulanMetod/AsalSayılarıBulanMetod/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsalSayılarıBulanMetod
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+     void kacGUndurYasiyor2(DateTime dT)
+        {
+            TimeSpan aradaFark = DateTime.Now - dT;
+
+        }
+       
+    void KacGundurYasiyor(DateTime dogumTarihi)
+    {
+        TimeSpan fark = DateTime.Now - dogumTarihi;
+        MessageBox.Show(fark.TotalDays.ToString());
+    }
+}
+}
